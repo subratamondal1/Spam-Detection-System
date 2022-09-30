63bf1846742d3778ef8777c3718f9cd667fa4614
Subrata Mondal <subratasubha2@gmail.com>
2022-09-30 12:50:52 +0530

    nltk.download('punkt')

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
 import streamlit as st
 import pickle
 import string
 import nltk
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
+nltk.download('punkt')
 
 # Initialize PorterStemmer()
 porterStemmer = PorterStemmer()
